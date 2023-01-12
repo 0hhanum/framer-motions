1718efdaee04583624eae8a6562e95d906378819
hanum <rntls123@naver.com>
2023-01-13 01:14:30 +0900

    animate gradient using motion value

--- src/Drag.tsx
+++ src/Drag.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import ScreenComponent, { IScreenRefProps } from "./ScreenComponent";
-import { forwardRef, useImperativeHandle, useRef, useState } from "react";
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
 
 const Wrapper = styled(motion.div)`
   width: 100%;
@@ -36,11 +42,54 @@ const Box = styled(motion.div)`
 const Drag = forwardRef<IScreenRefProps>(function Drag(props, ref) {
   const screenRef = useRef<IScreenRefProps>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
+  const xPos = useMotionValue(0);
+  const gradient = useTransform(
+    xPos,
+    [-50, 120],
+    [
+      `radial-gradient(
+      circle at 50% 0,
+      rgba(255, 0, 0, 0.5),
+      rgba(255, 0, 0, 0) 70.71%
+    ),
+    radial-gradient(
+      circle at 6.7% 75%,
+      rgba(0, 0, 255, 0.5),
+      rgba(0, 0, 255, 0) 70.71%
+    ),
+    radial-gradient(
+        circle at 93.3% 75%,
+        rgba(0, 255, 0, 0.5),
+        rgba(0, 255, 0, 0) 70.71%
+      )
+      beige`,
+      `radial-gradient(
+        circle at 50% 0,
+        rgb(4, 255, 0),
+        rgba(255, 0, 0, 0) 70.71%
+      ),
+      radial-gradient(
+        circle at 6.7% 75%,
+        rgb(255, 0, 0),
+        rgba(0, 0, 255, 0) 70.71%
+      ),
+      radial-gradient(
+          circle at 93.3% 75%,
+          rgb(0, 8, 255),
+          rgba(0, 255, 0, 0) 70.71%
+        )
+        beige`,
+    ]
+  );
   useImperativeHandle(ref, () => screenRef.current as IScreenRefProps);
   return (
     <ScreenComponent ref={screenRef}>
       <Wrapper ref={wrapperRef}>
-        <Box drag dragConstraints={wrapperRef}>
+        <Box
+          drag
+          dragConstraints={wrapperRef}
+          style={{ x: xPos, background: gradient }}
+        >
           DRAG ME
         </Box>
       </Wrapper>
