f4fbc4feff8398568a02376dce5817a55e9193b9
hanum <rntls123@naver.com>
2023-01-13 22:31:48 +0900

    Edit index.html

---
+++ src/Svg.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import ScreenComponent, { IScreenRefProps } from "./ScreenComponent";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
+
+const Wrapper = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to right, pink, green);
+  color: black;
+  font-weight: lighter;
+`;
+const wrapperVariants = {
+  hover: { borderRadius: "50%", scale: 1.5 },
+  click: { borderRadius: 0 },
+};
+const Gestures = forwardRef<IScreenRefProps>(function Gestures(props, ref) {
+  const [isHover, setIsHover] = useState(false);
+  const screenRef = useRef<IScreenRefProps>(null);
+  useImperativeHandle(ref, () => screenRef.current as IScreenRefProps);
+  const onHover = (isHover: boolean) => {
+    setIsHover(isHover);
+  };
+  return (
+    <ScreenComponent ref={screenRef}>
+      <Wrapper
+        variants={wrapperVariants}
+        whileTap="click"
+        whileHover="hover"
+        onMouseEnter={() => onHover(true)}
+        onMouseLeave={() => onHover(false)}
+      >
+        {isHover ? "CLICK ME" : "HOVER ME"}
+      </Wrapper>
+    </ScreenComponent>
+  );
+});
+
+export default Gestures;
